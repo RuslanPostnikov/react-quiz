b1c2b40bfec48c1c5869490ab9b5ad638a08733f
RuslanPostnikov <rp02081992@gmail.com>
2022-03-19 18:39:03 +0200

    Validation 2

--- src/containers/Auth.jsx
+++ src/containers/Auth.jsx
@@ -3,6 +3,14 @@ import styles from "./Auth.module.css";
 import Button from "../components/UI/Button";
 import Input from "../components/UI/Input";
 
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
 export default class Auth extends React.Component {
     constructor(props) {
         super(props);
@@ -49,8 +57,31 @@ export default class Auth extends React.Component {
         e.preventDefault();
     }
 
+    validateControl(value, validation) {
+        if(!validation) return true;
+
+        let isValid = true;
+
+        if(validation.required) isValid = value.trim() !== '' && isValid;
+        if(validation.email) isValid = validateEmail(value) && isValid;
+        if(validation.minLength) isValid = value.length >= validation.minLength && isValid;
+
+        return isValid
+    }
+
     onChangeHandler = (e, controlName) => {
-        console.log(`${controlName}: `, e.target.value)
+        console.log(`${controlName}: `, e.target.value);
+
+        const formControls = {...this.state.formControls};
+        const control = {...formControls[controlName]};
+
+        control.value = e.target.value;
+        control.touched = true;
+        control.valid = this.validateControl(control.value, control.validation);
+
+        formControls[controlName] = control;
+
+        this.setState({formControls});
     }
 
     renderInputs() {
