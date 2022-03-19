dc77c46750228e85683bde43d9c45f6b043b42d2
RuslanPostnikov <rp02081992@gmail.com>
2022-03-19 17:58:59 +0200

    Validation

--- src/containers/Auth.jsx
+++ src/containers/Auth.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import styles from "./Auth.module.css";
 import Button from "../components/UI/Button";
+import Input from "../components/UI/Input";
 
 export default class Auth extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formControls: {
+                email: {
+                    value: '',
+                    type: 'email',
+                    label: 'Email',
+                    errorMessage: 'Enter correct email',
+                    valid: false,
+                    touched: false,
+                    validation: {
+                        required: true,
+                        email: true
+                    }
+                },
+                password: {
+                    value: '',
+                    type: 'password',
+                    label: 'Password',
+                    errorMessage: 'Enter correct password',
+                    valid: false,
+                    touched: false,
+                    validation: {
+                        required: true,
+                        minLength: 6
+                    }
+                }
+            }
+        }
+    }
+
+
     logInHandler = () => {
 
     }
@@ -15,6 +49,29 @@ export default class Auth extends React.Component {
         e.preventDefault();
     }
 
+    onChangeHandler = (e, controlName) => {
+        console.log(`${controlName}: `, e.target.value)
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    errorMessage={control.errorMessage}
+                    shouldValidate={!!control.validation}
+                    onChange={e => this.onChangeHandler(e, controlName)}
+                />
+            )
+        })
+    }
+
 
     render() {
         return (
@@ -23,8 +80,7 @@ export default class Auth extends React.Component {
                     <h1>Auth</h1>
 
                     <form onSubmit={this.submitHandler} className={styles.AuthForm}>
-                        <input type="text"/>
-                        <input type="text"/>
+                        {this.renderInputs()}
                         <Button type='success' onClick={this.logInHandler}>Log in</Button>
                         <Button type='primary' onClick={this.signInHandler}>Sign in</Button>
                     </form>
