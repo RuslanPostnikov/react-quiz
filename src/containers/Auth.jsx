62ba9a92dc270abf477562ebcbf2f93e00fbb6d4
RuslanPostnikov <rp02081992@gmail.com>
2022-03-19 17:35:10 +0200

    Auth page

--- src/containers/Auth.jsx
+++ src/containers/Auth.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import styles from "./Auth.module.css";
+import Button from "../components/UI/Button";
 
 export default class Auth extends React.Component {
+    logInHandler = () => {
+
+    }
+
+    signInHandler = () => {
+
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault();
+    }
+
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={styles.Auth}>
+                <div>
+                    <h1>Auth</h1>
+
+                    <form onSubmit={this.submitHandler} className={styles.AuthForm}>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <Button type='success' onClick={this.logInHandler}>Log in</Button>
+                        <Button type='primary' onClick={this.signInHandler}>Sign in</Button>
+                    </form>
+                </div>
             </div>
         );
     }
